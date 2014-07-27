f492a1f2dc68d77ead2c64cf21561c267104a562
Nick Jablonski <nickjablonski@gmail.com>
2014-07-27 22:03:33 +0000

    made my site

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ var appHelper = require("./apphelper.js");
 appHelper.runApp(function(app, db) {
 	
 
-	console.log("Travel application is running!");
+  console.log("Travel application is running!");
 
-	var myName = "Nick";
-	app.get("/", function (req, resp) {
-		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
-		resp.end();
-	});
+  var myName = "Nick";
+  app.get("/", function (req, resp) {
+    resp.render("index", { "name": myName } );
+    resp.render("index", obj);
+  });
 
-})
+  app.get("/icecream/:flavor", function(req,resp) {
+    resp.render("icecream", {flavor: req.params.flavor});
+  });
+
+  app.get("/travel", function(req, resp) {
+    db.findArray({}, function(results) {
+      var params = {
+        "places": results
+      }
+      
+      resp.render("travel", params);
+    });
+   
+  });
+  
+  
+   app.get("/travel/rating/:rating", function(req, resp) {
+      var rating = parseInt(req.params.rating);
+      db.findArray({rating: rating }, function(results) {
+        var params = {
+          "places": results
+        }
+
+        resp.render("travel", params);
+      });
+   });
+});
